d43c7e4915bd139ad2485066a79480ead9f03bbe
Trevor Chapman <trevor.p.chapman@gmail.com>
2014-11-07 15:48:05 -0800

    Adding timer activity stub

---
+++ app/src/main/java/chapmant/materialtest/TeaTimingActivity.java
@@ -0,0 +1,21 @@
+package chapmant.materialtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by pan on 11/7/2014.
+ */
+public class TeaTimingActivity extends Activity {
+    Timer mTimer;
+    TimerTask mTimerTask;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
